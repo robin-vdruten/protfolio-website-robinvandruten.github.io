ca1875b012f20e367a73356a7bf6f43d7689bede
Robin van Druten <94444005+robin-vdruten@users.noreply.github.com>
2021-12-15 17:31:13 +0100

    update

--- js/main.js
+++ js/main.js
@@ -27,9 +27,19 @@ document.addEventListener("DOMContentLoaded", function () {
 //page loader with jquery
 
 $(document).ready(function () {
-  setTimeout(function () {
-    $("body").addClass("loaded");
-  }, 700);
+  if(window.location.pathname === '/index.html') {
+    setTimeout(function () {
+      $("body").addClass("loaded");
+    }, 700);
+  }
+});
+
+$(document).ready(function () {
+  if(window.location.pathname === '/about.html') {
+    setTimeout(function () {
+      $("body").addClass("loaded");
+    }, 5);
+  }
 });
 
 // page loader
